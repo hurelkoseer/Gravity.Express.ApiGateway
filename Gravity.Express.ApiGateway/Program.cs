72c33d6d0bc55716915e17fa5be347f4336939fe
hurel-koseer-alg <hurel.koseer@1000.com.tr>
2024-02-25 21:43:04 +0300

    configuration

--- Gravity.Express.ApiGateway/Program.cs
+++ Gravity.Express.ApiGateway/Program.cs
@@ -5,8 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOcelot();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
 var app = builder.Build();
+
+app.UseCors("CorsPolicy");
 
 app.UseOcelot();
 
