b891dc7e3d11de9ef64509167bc230f0ba5fa8d4
hurel-koseer-alg <hurel.koseer@1000.com.tr>
2024-02-26 04:06:12 +0300

    gateway configuration

--- Gravity.Express.ApiGateway/Program.cs
+++ Gravity.Express.ApiGateway/Program.cs
@@ -3,22 +3,51 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHealthChecks(); 
+builder.Services.AddHealthChecks();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = "Bearer";
+        options.DefaultChallengeScheme = "Bearer";
+    })
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "http://localhost:5005"; 
+        options.RequireHttpsMetadata = false;  
+        options.Audience = "gravity.express.api";  
+    });
+
 builder.Services.AddOcelot();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("CorsPolicy", builder =>
-        builder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader());
+    options.AddPolicy("CorsPolicy", builder => builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
 });
 
 var app = builder.Build();
 
-app.UseCors("CorsPolicy");
- 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
-app.UseOcelot();
+app.UseCors("CorsPolicy");  
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHealthChecks("/health");  
+});
+
+ 
+await app.UseOcelot();
 
 app.Run();
